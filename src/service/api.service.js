f6b769e604740fdd70678369122921384348af71
MartinKamenov <martinkamenov10@gmail.com>
2019-06-11 19:02:41 +0300

    Add login and register functions to api service

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const url = 'https://github-analyzator-api.herokuapp.com/';
+const url = 'https://rebuilder-api.herokuapp.com';
+const authPath = '/auth';
+const loginPath = '/login';
+const registerPath = '/register';
 
 const sendObject = {
     headers: {
@@ -9,7 +12,19 @@ const sendObject = {
 };
 
 const apiService = {
-    
+    login: (username, password) => {
+        const loginUrl = url + authPath + loginPath;
+        const body = { username, password };
+
+        return axios.post(loginUrl, body, sendObject);
+    },
+
+    register: (username, email, password) => {
+        const registerUrl = url + authPath + registerPath;
+        const body = { username, email, password };
+
+        return axios.post(registerUrl, body, sendObject);
+    }
 };
 
 module.exports = apiService;
