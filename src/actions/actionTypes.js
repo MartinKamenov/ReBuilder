ec8dcfbbf4805b6252dfd1b0633506c91bcdb127
MartinKamenov <martinkamenov10@gmail.com>
2019-06-20 21:32:05 +0300

    Add action types

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,8 @@
+export const BEGIN_AJAX_CALL = 'BEGIN_AJAX_CALL';
+export const AJAX_CALL_ERROR = 'AJAX_CALL_ERROR';
+
+export const LOGIN = 'LOGIN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+
+export const REGISTER = 'REGISTER';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
