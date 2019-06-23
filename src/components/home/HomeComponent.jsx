de2f1fafc0d044db86676684fee4ebadfb79ab7a
PavlovVasil <vasil@vasilpavlov.com>
2019-06-23 13:12:22 +0300

    Add initial user button

--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import './HomeComponent.css';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 class HomeComponent extends Component {
-    state = {  }
+    state = { 
+        user: {
+            id: 'someId',
+            username: 'testUsername',
+            password: 'somepass',
+            email: 'test@abv.bg',
+            imageUrl: 'https://avatars3.githubusercontent.com/u/18643062?s=400&u=1eb73de3d53dd1589827b68095ca18cf6b2518fc&v=4',
+            projects: [
+                {
+                    name: 'test1',
+                    userName: 'vasko',
+                    userId: 'someId',
+                    projectImageUrl: 'https://www.image-line.com/support/flstudio_online_manual/html/img_shot/browser.png'
+                }
+            ]
+        }
+     }
     render() { 
         return ( 
             <div>
                 <nav class="navbar" id="user-navbar">
-                    <a class="navbar-brand" href="#">
-                        <img src="https://mdbootstrap.com/img/logo/mdb-transparent.png" height="30" alt="mdb logo" />
+                    <button class="user-profile">
+                    <a class="navbar-brand user-image" href="#">
+                        <img src={this.state.user.imageUrl} height="100%" alt="user avatar" />
                     </a>
+                    <div class="username">{this.state.user.username}
+                        <FontAwesomeIcon icon={faCaretDown} /> 
+                    </div>
+                    </button>
                 </nav>
             </div>
         );
