749df602af6ded61aa4ed918aa0a3ddc572ca518
MartinKamenov <martinkamenov10@gmail.com>
2019-06-28 19:46:14 +0300

    Add actions for creating project

--- src/components/project/newProjectPage/NewProjectPageComponent.jsx
+++ src/components/project/newProjectPage/NewProjectPageComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './NewProjectPageComponent.css';
 
 class NewProjectPageComponent extends Component {
     state = { 
@@ -11,19 +12,25 @@ class NewProjectPageComponent extends Component {
     }
     render() { 
         return ( 
-            <div>
-                <label>Project name</label>
-                <input
-                    className='new-project-name-input'
-                    placeholder='Add name here'
-                    value={this.state.name}
-                    onChange={(event) => this.handleChangeInput(event.target.value, 'name')}/>
-                <label>Project image</label>
-                <input
-                    className='new-project-name-input'
-                    placeholder='Add name here'
-                    value={this.state.projectImage}
-                    onChange={(event) => this.handleChangeInput(event.target.value, 'projectImage')}/>
+            <div className='center-container'>
+                <div>
+                    <label className='project-page-label'>Project name</label>
+                    <input
+                        className='project-page-input'
+                        placeholder='Add name here'
+                        value={this.state.name}
+                        onChange={(event) => this.handleChangeInput(event.target.value, 'name')}/>
+                    
+                </div>
+                <div>
+                    <label className='project-page-label'>Project image</label>
+                    <input
+                        className='project-page-input'
+                        placeholder='Add url here'
+                        value={this.state.projectImage}
+                        onChange={(event) => this.handleChangeInput(event.target.value, 'projectImage')}/>
+                </div>
+                <button className='btn btn-success project-page-button'>Create new project</button>
             </div>
         );
     }
