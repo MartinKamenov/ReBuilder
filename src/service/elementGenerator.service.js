5d1e50b7fbda2046e6d5ea6984734e4ef815a7e6
MartinKamenov <martinkamenov10@gmail.com>
2019-07-03 22:05:56 +0300

    Generate styles for components

--- src/service/elementGenerator.service.js
+++ src/service/elementGenerator.service.js
@@ -4,16 +4,20 @@ const elementGenerator = {
         droppedComponents.forEach(component => {
             switch(component.name) {
                 case 'Header':
-                    result += `<h1 className='element-center'>${component.innerText}</h1>`;
+                    result += 
+`<h1 className='element-center' style={${JSON.stringify(component.style)}}>${component.innerText}</h1>`;
                     break;
                 case 'Text':
-                    result += `<div className='element-center'>${component.innerText}</div>`;
+                    result += 
+`<div className='element-center' style={${JSON.stringify(component.style)}}>${component.innerText}</div>`;
                     break;
                 case 'Grid':
-                    result += `<div className='element-center'>${component.innerText}</div>`;
+                    result += 
+`<div className='element-center' style={${JSON.stringify(component.style)}}>${component.innerText}</div>`;
                     break;
                 default:
-                    result += `<div className='element-center'>${component.innerText}</div>`;
+                    result += 
+`<div className='element-center' style={${JSON.stringify(component.style)}}>${component.innerText}</div>`;
                     break;
             }
         });
