30279d8564d2d6754fb05aa88559140fecdec922
MartinKamenov <martinkamenov10@gmail.com>
2019-07-08 20:35:54 +0300

    Add actions and reducers for projectStatus

---
+++ src/actions/deploymentActions.js
@@ -0,0 +1,23 @@
+import * as types from './actionTypes';
+import apiService from '../service/api.service';
+import { error as toastError } from 'react-toastify-redux';
+import { createError } from './errorActions';
+
+export function deployProject(projectId, token) {
+    return async function(dispatch) {
+        try {
+            let res;
+            await apiService.deployProject(projectId, token);
+            res = await apiService.getProject(projectId, token);
+            const projectStatus = res.data;
+            return dispatch(deployProjectSuccess(projectStatus));
+        } catch(error) {
+            dispatch(toastError(error.message));
+            return dispatch(createError(error.message));
+        }
+    };
+}
+
+export function deployProjectSuccess(projectStatus) {
+    return { type: types.DEPLOY_PROJECT_SUCCESS, projectStatus };
+}
---
+++ src/reducers/deployProjectReducer.js
@@ -0,0 +1,11 @@
+import * as types from '../actions/actionTypes';
+
+export default function deployProject(projectStatus = null, action) {
+    switch (action.type) {
+        case types.DEPLOY_PROJECT_SUCCESS:
+            return action.projectStatus;
+
+        default:
+            return projectStatus;
+    }
+};
