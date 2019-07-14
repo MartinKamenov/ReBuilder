4d9b7b81ec86fc6c08f8201475b6c8f3a7c65ca7
MartinKamenov <martinkamenov10@gmail.com>
2019-07-14 13:45:49 +0300

    Update pages via component state

--- src/components/project/innerRouting/InnerRoutingComponent.jsx
+++ src/components/project/innerRouting/InnerRoutingComponent.jsx
@@ -7,8 +7,12 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import './InnerRoutingComponent.css';
 import LoadingComponent from '../../common/LoadingComponent';
+import uuid from 'uuid';
 
 class InnerRoutingComponent extends Component {
+    state = {
+        pages: []
+    }
     componentDidMount() {
         const token = localStorage.getItem('token');
         if(!this.props.user.id && !token) {
@@ -20,23 +24,59 @@ class InnerRoutingComponent extends Component {
         this.setState({ id });
         this.props.actions.updateProject(id, null, token);
     }
+
+    componentWillReceiveProps(props) {
+        if(props.project.pages) {
+            this.setState({ pages: props.project.pages });
+        }
+    }
+
+    navigateToPage = (pageId) => {
+        const project = this.props.project;
+        this.setState({ isLoading: false });
+        const history = this.props.history;
+        history.push(`/${project.id}/${pageId}`);
+    }
+
+    addNewPage = () => {
+        const page = {
+            uuid: uuid.v1(),
+            route: '/routing',
+            elements: []
+        };
+
+        const project = this.props.project;
+        project.pages.push(page);
+
+        this.props.actions.updateProject(project.id, project.pages, this.props.user.id);
+        this.setState({ pages: project.pages });
+    }
+
     render() {
         if(!this.props.project.id) {
-            return (<LoadingComponent message='Fetching project'/>)
+            return (<LoadingComponent message='Fetching project'/>);
         }
         return (
             <div>
                 {
-                    this.props.project.pages.map(() => {
-                        return (<div className='col-md-4 routing-page-component'>
-                            Pesho
-                        </div>);
+                    this.state.pages.map((page) => {
+                        return (
+                            <div key={page.id} className='col-md-4 col-sm-6 outer-route-container'>
+                                <div className='routing-page-component'>
+                                    {page.route}
+                                </div>
+                            </div>
+                        );
                     })
                 }
 
-                <div className='col-md-4 routing-page-component add-route-container'>
-                    <h3>Add new page</h3>
-                    <FontAwesomeIcon style={{width: 100, height: 100}} icon={faPlusCircle} />
+                <div className='col-md-4 col-sm-6 outer-route-container'>
+                    <div
+                        className='routing-page-component add-route-container'
+                        onClick={this.addNewPage}>
+                        <h3>Add new page</h3>
+                        <FontAwesomeIcon style={{width: 100, height: 100}} icon={faPlusCircle} />
+                    </div>
                 </div>
             </div>
         );
