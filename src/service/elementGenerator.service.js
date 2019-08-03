6f56bc604aa8f1c9e6dd04ecd7143e8d2c0e6269
MartinKamenov <martinkamenov10@gmail.com>
2019-08-03 13:34:30 +0300

    Switch pixels to percentage

--- src/service/elementGenerator.service.js
+++ src/service/elementGenerator.service.js
@@ -1,39 +1,47 @@
 import { componentTypes } from '../components/project/components/componentTypes';
+const screenWidth = 1200;
+
 const getElement = (component) => {
+    const copyOfStyle = Object.assign({}, component.style);
+    if(copyOfStyle.width.endsWith('px')) {
+        const pixelsWidth = parseInt(copyOfStyle.width, 10);
+        delete copyOfStyle.width;
+        copyOfStyle.width = parseFloat((pixelsWidth / screenWidth) * 100) + '%';
+    }
     switch(component.name) {
     case componentTypes.Header:
         return ( 
-            `<h1 className='element-center' style={${JSON.stringify(component.style)}}>${component.innerText}</h1>`
+            `<h1 className='element-center' style={${JSON.stringify(copyOfStyle)}}>${component.innerText}</h1>`
         );
     case componentTypes.Text:
         return ( 
-            `<div className='element-center' style={${JSON.stringify(component.style)}}>${component.innerText}</div>`
+            `<div className='element-center' style={${JSON.stringify(copyOfStyle)}}>${component.innerText}</div>`
         );
     case componentTypes.Grid:
         return ( 
-            `<div className='element-center' style={${JSON.stringify(component.style)}}>${component.innerText}</div>`
+            `<div className='element-center' style={${JSON.stringify(copyOfStyle)}}>${component.innerText}</div>`
         );
     case componentTypes.Input:
         return ( 
-            `<input className='element-center' style={${JSON.stringify(component.style)}}/>`
+            `<input className='element-center' style={${JSON.stringify(copyOfStyle)}}/>`
         );
     case componentTypes.Image:
         return ( 
-            `<img alt='element' src={${JSON.stringify(component.src)}} className='element-center' style={${JSON.stringify(component.style)}}/>`
+            `<img alt='element' src={${JSON.stringify(component.src)}} className='element-center' style={${JSON.stringify(copyOfStyle)}}/>`
         );
     case componentTypes.RoutingLink:
         return ( 
-            `<Link to='${component.to}' className='element-center' style={${JSON.stringify(component.style)}}>${component.innerText}</Link>`
+            `<Link to='${component.to}' className='element-center' style={${JSON.stringify(copyOfStyle)}}>${component.innerText}</Link>`
         );
     case componentTypes.Container:
         return ( 
-            `<div className='element-center' style={${JSON.stringify(component.style)}}>
+            `<div className='element-center' style={${JSON.stringify(copyOfStyle)}}>
 ${component.children.map(c => getElement(c)).join('\n')}
 </div>`
         );
     default:
         return (
-            `<div className='element-center' style={${JSON.stringify(component.style)}}>${component.innerText}</div>`
+            `<div className='element-center' style={${JSON.stringify(copyOfStyle)}}>${component.innerText}</div>`
         );
     }
 };
