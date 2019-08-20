f4b060b827ccc8f342e62b1084c4c544dc9f4781
MartinKamenov <martinkamenov10@gmail.com>
2019-08-20 21:53:16 +0300

    Add updating page information in inputs

--- src/components/project/innerRouting/InnerRoutingComponent.jsx
+++ src/components/project/innerRouting/InnerRoutingComponent.jsx
@@ -183,6 +183,16 @@ class InnerRoutingComponent extends Component {
         }
     }
 
+    selectPage = (id) => {
+        const pages = [...this.state.pages];
+        const page = Object.assign({}, pages.find(p => p.id === id));
+        this.setState({
+            updatePage: page,
+            newPageName: page.name,
+            newPageRoute: page.route
+        });
+    }
+
     isValidClass = (errorField) => {
         if(this.state[errorField]) {
             return 'routing-form-input-invalid';
@@ -250,7 +260,7 @@ class InnerRoutingComponent extends Component {
                                         type='danger'
                                         onClick={this.changeUpdateStatus}
                                         className='routing-from-button'>
-                                            Cancel
+                                            Cancel update
                                     </ButtonComponent>
                                 </>
                             ) : (
@@ -281,15 +291,22 @@ class InnerRoutingComponent extends Component {
                             <li
                                 key={page.id}
                                 className='routing-page-styling-li'>
-                                <Link
+                                <div
                                     className='normal'
-                                    to={`/projects/${this.props.project.id}/${page.id}`}>
-                                <svg viewBox='0 0 80 76' x='0px' y='0px'>
-                                    <g>
-                                    <path d='M 68.9708 24.8623 L 60.4554 2.3018 C 59.9641 0.7017 58.1628 -0.2583 56.5252 0.3817 L 1.9822 20.2222 C 0.3822 20.7022 -0.4179 22.6222 0.2222 24.2223 L 8.8624 47.7797 L 8.8624 35.1484 C 8.8624 29.5024 13.3425 24.8623 18.8857 24.8623 L 32.9442 24.8623 L 50.63 12.862 L 60.7829 24.8623 L 68.9708 24.8623 L 68.9708 24.8623 ZM 77.098 32.0625 L 18.8857 32.0625 C 17.2512 32.0625 16.0625 33.4511 16.0625 35.1484 L 16.0625 72.8491 C 16.0625 74.5477 17.2512 75.9375 18.8857 75.9375 L 77.098 75.9375 C 78.742 75.9375 79.9376 74.5477 79.9376 72.8491 L 79.9376 35.1484 C 79.9376 33.4511 78.742 32.0625 77.098 32.0625 L 77.098 32.0625 ZM 73.0626 68.0625 L 23.9375 68.0625 L 23.9375 61.0852 L 31.4704 43.7232 L 42.7696 57.6777 L 53.4138 46.8062 L 67.1695 41.9375 L 73.0626 55.0815 L 73.0626 68.0625 L 73.0626 68.0625 Z' />
-                                    </g>
-                                </svg>
-                                </Link>
+                                    onClick={(event) => {
+                                        if(this.state.isUpdating) {
+                                            this.selectPage(page.id);
+                                            return;
+                                        }
+
+                                        this.navigateToPage(page.id);
+                                    }}>
+                                    <svg viewBox='0 0 80 76' x='0px' y='0px'>
+                                        <g>
+                                        <path d='M 68.9708 24.8623 L 60.4554 2.3018 C 59.9641 0.7017 58.1628 -0.2583 56.5252 0.3817 L 1.9822 20.2222 C 0.3822 20.7022 -0.4179 22.6222 0.2222 24.2223 L 8.8624 47.7797 L 8.8624 35.1484 C 8.8624 29.5024 13.3425 24.8623 18.8857 24.8623 L 32.9442 24.8623 L 50.63 12.862 L 60.7829 24.8623 L 68.9708 24.8623 L 68.9708 24.8623 ZM 77.098 32.0625 L 18.8857 32.0625 C 17.2512 32.0625 16.0625 33.4511 16.0625 35.1484 L 16.0625 72.8491 C 16.0625 74.5477 17.2512 75.9375 18.8857 75.9375 L 77.098 75.9375 C 78.742 75.9375 79.9376 74.5477 79.9376 72.8491 L 79.9376 35.1484 C 79.9376 33.4511 78.742 32.0625 77.098 32.0625 L 77.098 32.0625 ZM 73.0626 68.0625 L 23.9375 68.0625 L 23.9375 61.0852 L 31.4704 43.7232 L 42.7696 57.6777 L 53.4138 46.8062 L 67.1695 41.9375 L 73.0626 55.0815 L 73.0626 68.0625 L 73.0626 68.0625 Z' />
+                                        </g>
+                                    </svg>
+                                </div>
                                 <div className='info'>
                                     <h3 className='routing-page-styling-h3'>{page.name}</h3>
                                     <p>{page.route}</p>
