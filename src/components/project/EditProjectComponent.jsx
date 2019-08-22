40873458f881fea4ac854039bfc30bc634cced19
MartinKamenov <martinkamenov10@gmail.com>
2019-08-22 21:48:19 +0300

    Use copy of dropped elements for edit mode changes

--- src/components/project/EditProjectComponent.jsx
+++ src/components/project/EditProjectComponent.jsx
@@ -265,7 +265,7 @@ class EditProjectComponent extends Component {
     }
     getComponentInEditMode = () => {
         let index = -1;
-        const droppedComponents = this.state.droppedComponents;
+        const droppedComponents = [...this.state.droppedComponents];
         let component = droppedComponents.find((c, i) => {
             if(c.isInEditMode) {
                 index = i;
