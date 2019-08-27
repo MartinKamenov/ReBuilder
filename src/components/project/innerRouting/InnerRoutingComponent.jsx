866ba97b74f962133dae4949ecdb38b88ef37ccc
MartinKamenov <martinkamenov10@gmail.com>
2019-08-27 22:21:27 +0300

    Add method for showing page elements

--- src/components/project/innerRouting/InnerRoutingComponent.jsx
+++ src/components/project/innerRouting/InnerRoutingComponent.jsx
@@ -12,6 +12,7 @@ import './InnerRoutingComponent.css';
 import './PageElementsStyle.css';
 import ButtonComponent from '../../common/ButtonComponent';
 import ProjectActionButtonsComponent from '../../common/ProjectActionButtonsComponent';
+import { componentTypes } from '../components/componentTypes';
 
 class InnerRoutingComponent extends Component {
     state = {
@@ -244,6 +245,13 @@ class InnerRoutingComponent extends Component {
         }
     }
 
+    getComponentJSX = (component) => {
+        switch(component.name) {
+            case componentTypes.Image:
+                return <img src={component.src} style={component.style}/>
+        }
+    }
+
     render() {
         if(this.state.isLoading) {
             return (<LoadingComponent message='Fetching project'/>);
@@ -357,7 +365,7 @@ class InnerRoutingComponent extends Component {
                                     }>
                                 <div
                                     className='normal'
-                                    onClick={(event) => {
+                                    onClick={() => {
                                         if(this.state.isUpdating) {
                                             this.selectPage(page.id);
                                             return;
@@ -365,11 +373,12 @@ class InnerRoutingComponent extends Component {
 
                                         this.navigateToPage(page.id);
                                     }}>
-                                    <svg viewBox='0 0 80 76' x='0px' y='0px'>
-                                        <g>
-                                        <path d='M 68.9708 24.8623 L 60.4554 2.3018 C 59.9641 0.7017 58.1628 -0.2583 56.5252 0.3817 L 1.9822 20.2222 C 0.3822 20.7022 -0.4179 22.6222 0.2222 24.2223 L 8.8624 47.7797 L 8.8624 35.1484 C 8.8624 29.5024 13.3425 24.8623 18.8857 24.8623 L 32.9442 24.8623 L 50.63 12.862 L 60.7829 24.8623 L 68.9708 24.8623 L 68.9708 24.8623 ZM 77.098 32.0625 L 18.8857 32.0625 C 17.2512 32.0625 16.0625 33.4511 16.0625 35.1484 L 16.0625 72.8491 C 16.0625 74.5477 17.2512 75.9375 18.8857 75.9375 L 77.098 75.9375 C 78.742 75.9375 79.9376 74.5477 79.9376 72.8491 L 79.9376 35.1484 C 79.9376 33.4511 78.742 32.0625 77.098 32.0625 L 77.098 32.0625 ZM 73.0626 68.0625 L 23.9375 68.0625 L 23.9375 61.0852 L 31.4704 43.7232 L 42.7696 57.6777 L 53.4138 46.8062 L 67.1695 41.9375 L 73.0626 55.0815 L 73.0626 68.0625 L 73.0626 68.0625 Z' />
-                                        </g>
-                                    </svg>
+                                    <div>
+                                        {
+                                            page.elements
+                                                .map((component) => this.getComponentJSX(component))
+                                        }
+                                    </div>
                                 </div>
                                 <div className='info'>
                                     <h3 className='routing-page-styling-h3'>{page.name}</h3>
