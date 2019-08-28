b45d1434a9e9c7452373a08ccef62e89e17c5fb9
MartinKamenov <martinkamenov10@gmail.com>
2019-08-28 21:11:59 +0300

    Add services component

---
+++ src/components/services/ServicesComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './ServicesComponent.css';
+
+class ServicesComponent extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>Services</div>
+        );
+    }
+}
+ 
+export default ServicesComponent;
