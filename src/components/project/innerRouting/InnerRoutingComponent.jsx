ef5b3ce8015ad1ca8abd7a7bd7bb01f400311994
MartinKamenov <martinkamenov10@gmail.com>
2019-11-08 17:16:09 +0200

    Update folder structure for common components

--- src/components/project/innerRouting/InnerRoutingComponent.jsx
+++ src/components/project/innerRouting/InnerRoutingComponent.jsx
@@ -4,14 +4,14 @@ import * as deploymentActions from '../../../actions/deploymentActions';
 import * as getDeploymentActions from '../../../actions/getDeploymentActions';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import LoadingComponent from '../../common/LoadingComponent';
+import LoadingComponent from '../../common/loading-page/LoadingComponent';
 import uuid from 'uuid';
 import projectGenerator from '../../../service/projectGenerator.service';
 import SaveStatus from '../components/saveStatus';
 
 import './InnerRoutingComponent.css';
 import './PageElementsStyle.css';
-import ProjectActionButtonsComponent from '../../common/ProjectActionButtonsComponent';
+import ProjectActionButtonsComponent from '../../common/project-actions-buttons/ProjectActionButtonsComponent';
 import { componentTypes } from '../components/componentTypes';
 import ProjectPageComponent from './tabs/pageTab/PagesTabComponent';
 import PagesEditorComponent from './tabs/pageTab/PagesEditorComponent';
