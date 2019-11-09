5c91c5fc30ce35f020873c0f70985eb045ed7acb
MartinKamenov <martinkamenov10@gmail.com>
2019-11-09 14:11:12 +0200

    Remove services for generating file templates

--- src/service/projectGenerator.service.js
+++ src/service/projectGenerator.service.js
@@ -1,19 +1,7 @@
 import JSZip from 'jszip';
 import FileSaver from 'file-saver';
-import templatingService from './templating.service';
 
 const projectGenerator = {
-    generateProject: (name, pages, imageUrl) => {
-        let zip = new JSZip();
-        const templates = templatingService.getAllTemplates(name, pages, imageUrl);
-        templates.forEach((templateObject) => {
-            zip.file(templateObject.filePath, templateObject.template);
-        });
-        zip.generateAsync({type: 'blob'}).then(function(content) {
-            FileSaver.saveAs(content, `${name}.zip`);
-        });
-    },
-
     generateProjectFiles: (templates, name) => {
         let zip = new JSZip();
 
