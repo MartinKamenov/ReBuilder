5b721ab4a80f039e06f956f0b449fc75d038281b
MartinKamenov <martinkamenov10@gmail.com>
2019-11-13 16:02:41 +0200

    Add material UI and update button component

--- src/components/common/button/ButtonComponent.jsx
+++ src/components/common/button/ButtonComponent.jsx
@@ -1,41 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './ButtonComponent.css';
+import Button from '@material-ui/core/Button';
 
-const ButtonComponent = ({ title, style, type, onClick, className, rounded, children }) => {
+const ButtonComponent = ({
+    title,
+    style,
+    color,
+    variant,
+    type,
+    onClick,
+    className,
+    rounded,
+    children
+}) => {
     if(rounded === undefined) {
         rounded = true;
     }
 
     if(children && children.length > 0) {
         return (
-            <button
+            <Button
+                variant={variant}
+                color={color}
                 style={style}
                 className={
-                    `custom-button ${className} custom-button-${type} ${rounded ? 'custom-buttom-rounded':''}`
+                    `custom-button ${className} ${rounded ? 'custom-buttom-rounded':''}`
                 }
                 onClick={onClick}>
                 {children}
-            </button>
+            </Button>
         );
     }
 
     return (
-        <button
+        <Button
+            className={className}
             style={style}
-            className={
-                `custom-button ${className} custom-button-${type} ${rounded ? 'custom-buttom-rounded':''}`
-            }
+            variant={variant}
+            color={color}
             onClick={onClick}>
             {title}
-        </button>
+        </Button>
     );
 };
 
 ButtonComponent.propTypes = {
     title: PropTypes.string,
     style: PropTypes.object,
-    type: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    variant: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
     className: PropTypes.string,
     rounded: PropTypes.bool
