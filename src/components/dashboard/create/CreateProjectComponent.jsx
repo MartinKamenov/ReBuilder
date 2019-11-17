20cf5ee50670ba120935c1ee613804c9d9d2895c
MartinKamenov <martinkamenov10@gmail.com>
2019-11-17 19:24:17 +0200

    Add multyline input functionality

--- src/components/dashboard/create/CreateProjectComponent.jsx
+++ src/components/dashboard/create/CreateProjectComponent.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
@@ -11,48 +10,52 @@ import Typography from '@material-ui/core/Typography';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import ButtonComponent from '../../common/button/ButtonComponent';
+import InputComponent from '../../common/input/InputComponent';
+import './CreateProjectComponent.css';
 
 const styles = theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
+    root: {
+        margin: 0,
+        padding: theme.spacing(2),
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    },
 });
 
 const DialogTitle = withStyles(styles)(props => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
+    const { children, classes, onClose, ...other } = props;
+    return (
+        <MuiDialogTitle disableTypography className={classes.root} {...other}>
+            <Typography variant='h6'>{children}</Typography>
+            {onClose ? (
+                <IconButton aria-label='close' className={classes.closeButton} onClick={onClose}>
+                    <CloseIcon />
+                </IconButton>
+            ) : null}
+        </MuiDialogTitle>
+    );
 });
 
 const DialogContent = withStyles(theme => ({
-  root: {
-    padding: theme.spacing(2),
-  },
+    root: {
+        padding: theme.spacing(2),
+    },
 }))(MuiDialogContent);
 
 const DialogActions = withStyles(theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
+    root: {
+        margin: 0,
+        padding: theme.spacing(1),
+    },
 }))(MuiDialogActions);
 const CreateProjectComponent = () => {
     const [open, setOpen] = useState(false);
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -62,49 +65,51 @@ const CreateProjectComponent = () => {
     };
     return (
         <div className='center-container' style={{ marginTop: '-30px', marginBottom: '10px' }}>
-        <ButtonComponent
-                    style={{
-                        width: 200,
-                        fontWeight: 'bold',
-                        fontSize: 12,
-                        height: 50,
-                        marginLeft: 'auto',
-                        marginRight: 'auto'
-                    }}
-                    variant='outlined'
-                    color='secondary'
-                    className='vertical-centered'
-                    onClick={handleClickOpen}>
-            <FontAwesomeIcon className='action-icon' icon={faPlusCircle} />
+            <ButtonComponent
+                style={{
+                    width: 200,
+                    fontWeight: 'bold',
+                    fontSize: 12,
+                    height: 50,
+                    marginLeft: 'auto',
+                    marginRight: 'auto'
+                }}
+                variant='outlined'
+                color='secondary'
+                className='vertical-centered'
+                onClick={handleClickOpen}>
+                <FontAwesomeIcon className='action-icon' icon={faPlusCircle} />
             Create new project
-        </ButtonComponent>
-        <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-            <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+            </ButtonComponent>
+            <Dialog fullWidth onClose={handleClose} aria-labelledby='customized-dialog-title' open={open}>
+                <DialogTitle id='customized-dialog-title' onClose={handleClose}>
             Create new project
-            </DialogTitle>
-            <DialogContent dividers>
-            <Typography gutterBottom>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-                in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-            </Typography>
-            <Typography gutterBottom>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-                lacus vel augue laoreet rutrum faucibus dolor auctor.
-            </Typography>
-            <Typography gutterBottom>
-                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-                scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-                auctor fringilla.
-            </Typography>
-            </DialogContent>
-            <DialogActions>
-            <Button autoFocus onClick={handleClose} color="primary">
-                Create project
-            </Button>
-            </DialogActions>
-        </Dialog>
+                </DialogTitle>
+                <DialogContent dividers>
+                    <InputComponent
+                        className='form-input'
+                        autoFocus
+                        placeholder='Project name'
+                        value={name}
+                        onChange={({target: {value}}) => setName(value)} />
+                    <InputComponent
+                        multiline
+                        className='form-input'
+                        rows='4'
+                        variant='filled'
+                        autoFocus
+                        placeholder='Description'
+                        value={description}
+                        onChange={({target: {value}}) => setDescription(value)} />
+                </DialogContent>
+                <DialogActions>
+                    <ButtonComponent style={{ fontSize: 12 }} onClick={handleClose} color='secondary'>
+                        Create project
+                    </ButtonComponent>
+                </DialogActions>
+            </Dialog>
         </div>
     );
-}
+};
  
 export default CreateProjectComponent;
